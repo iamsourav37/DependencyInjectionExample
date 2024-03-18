a59d68d60e93cc7c05fd371d6fbb898b2a9e7af7
iamsourav37 <sourav.g29@outlook.com>
2024-03-18 21:28:28 +0530

    tightly coupled service is added

--- DependencyInjectionExample/Controllers/CityController.cs
+++ DependencyInjectionExample/Controllers/CityController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services;
 
 namespace DependencyInjectionExample.Controllers
 {
     public class CityController : Controller
     {
+
+        private readonly CityService _cityService;
+
+        public CityController()
+        {
+            _cityService = new CityService();
+        }
         public IActionResult Index()
         {
-            return View();
+            var listOfCities = _cityService.GetCities();
+
+            return View(listOfCities);
         }
     }
 }
