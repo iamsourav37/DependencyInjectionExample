13aa5b5c5d730576fa5320181c0515733b8a719e
iamsourav37 <sourav.g29@outlook.com>
2024-03-18 21:23:05 +0530

    Add project files.

---
+++ DependencyInjectionExample/Controllers/CityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjectionExample.Controllers
+{
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Services/CityService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+    public class CityService
+    {
+
+        private List<string> _cities;
+
+        public CityService()
+        {
+            _cities = new List<string>();
+        }
+        public List<string> GetCities()
+        {
+            _cities.Add("Kolkata");
+            _cities.Add("Mumbai");
+            _cities.Add("Chennai");
+            _cities.Add("Delhi");
+            _cities.Add("Bengaluru");
+            _cities.Add("Hyderabad");
+
+            return _cities;
+        }
+    }
+}
